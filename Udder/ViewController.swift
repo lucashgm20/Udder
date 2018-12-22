e9a2b6a9c5694322d5a71a2b8ef999b1aea085f9
Lucas Moura <lucasmoura@lucass-macbook-pro.local>
2018-12-22 21:56:52 -0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Lucas Moura on 22/12/18.
+//  Copyright © 2018 Lucas Moura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
